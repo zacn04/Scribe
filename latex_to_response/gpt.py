f7a77439f682bfbe327dea88cdc5039c8c0aaccd
Will Reed <willreed@dhcp-10-29-182-154.dyn.mit.edu>
2023-09-17 10:18:20 -0400

    finished help window

--- latex_to_response/gpt.py
+++ latex_to_response/gpt.py
@@ -1,9 +1,12 @@
 import os
 import openai
 
-#  ̶i̶n̶ ̶p̶r̶o̶g̶r̶e̶s̶s̶ complete!
+from dotenv import load_dotenv
 
-key = "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"
+load_dotenv()
+key = os.getenv("OPENAI_KEY")
+
+#  ̶i̶n̶ ̶p̶r̶o̶g̶r̶e̶s̶s̶ complete!
 #openai.organization = "org-WbwlG5hvut8bXxTOfEm4tfV6"
 openai.api_key = key
 # openai.Model.list()
@@ -23,24 +26,24 @@ def explanation(prompt : str, answer : str) -> str:
   return response["choices"][0]["message"]["content"]
 
 # Testing 
+if __name__ == "__main__":
+  print(explanation('\\( \\int_{\\pi / 4}^{\\pi / 2} \\sin x d x \\)','1/sqrt(2)≈0.70711'))
+  '''
+  To find the exact value of the integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \), we need to evaluate the definite integral. Here are the detailed steps:
 
-print(explanation('\\( \\int_{\\pi / 4}^{\\pi / 2} \\sin x d x \\)','1/sqrt(2)≈0.70711'))
-'''
-To find the exact value of the integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \), we need to evaluate the definite integral. Here are the detailed steps:
+  Step 1: Start with the definite integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \).
 
-Step 1: Start with the definite integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \).
+  Step 2: Integrate \(\sin x\) with respect to \(x\). The antiderivative of \(\sin x\) is \(-\cos x\), so we have \(-\cos x\) as the indefinite integral.
 
-Step 2: Integrate \(\sin x\) with respect to \(x\). The antiderivative of \(\sin x\) is \(-\cos x\), so we have \(-\cos x\) as the indefinite integral.
+  Step 3: Evaluate the definite integral by subtracting the antiderivative at the upper bound (\(\pi / 2\)) from the antiderivative at the lower bound (\(\pi / 4\)). 
 
-Step 3: Evaluate the definite integral by subtracting the antiderivative at the upper bound (\(\pi / 2\)) from the antiderivative at the lower bound (\(\pi / 4\)). 
+  Step 4: Plugging in the values, we get \(-\cos(\pi / 2) - (-\cos(\pi / 4))\).
 
-Step 4: Plugging in the values, we get \(-\cos(\pi / 2) - (-\cos(\pi / 4))\).
+  Step 5: Simplify further. The cosine of \(\pi / 2\) is 0, and the cosine of \(\pi / 4\) is \(\sqrt{2}/2\). Therefore, we have \(0 - (-\sqrt{2}/2)\).
 
-Step 5: Simplify further. The cosine of \(\pi / 2\) is 0, and the cosine of \(\pi / 4\) is \(\sqrt{2}/2\). Therefore, we have \(0 - (-\sqrt{2}/2)\).
+  Step 6: Simplify to obtain \(\sqrt{2}/2\).
 
-Step 6: Simplify to obtain \(\sqrt{2}/2\).
+  Step 7: Since \(\sqrt{2}/2\) is an irrational number, we can approximate it as \(0.70711\) using decimal notation.
 
-Step 7: Since \(\sqrt{2}/2\) is an irrational number, we can approximate it as \(0.70711\) using decimal notation.
-
-Therefore, the value of the integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \) is approximately equal to \(1/\sqrt{2} \approx 0.70711\).
-'''
+  Therefore, the value of the integral \( \int_{\pi / 4}^{\pi / 2} \sin x \, dx \) is approximately equal to \(1/\sqrt{2} \approx 0.70711\).
+  '''
