65fefd288269a9c64a0a08506bf269128dd68b89
Zac <108872007+zacn04@users.noreply.github.com>
2023-09-17 02:10:04 -0400

    Update api.py

--- zac/api.py
+++ zac/api.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-
 #This code makes an API request to Mathpix, which can generate LaTeX from handwritten images.
 url = 'https://api.mathpix.com/v3/text'
 headers = {
@@ -13,18 +12,16 @@ options = {
     'formats': ['text', 'data', 'html']
 }
 
+def image_to_latex(img: str) -> str:
+    print(img)
+    with open(img, "rb") as img_file:
+        files = {'file': img_file}
+        data = {'options_json': json.dumps(options)}
+        response = requests.post(url, headers=headers, files=files, data=data)
 
-
-with open("Image.jpg", "rb") as img_file:
-    files = {'file': img_file}
-    data = {'options_json': json.dumps(options)}
-    response = requests.post(url, headers=headers, files=files, data=data)
-
-
-# Process the response
-if response.status_code == 200:
-    result = json.loads(response.text)
-    print("Text: ", result.get('text'))
-    # Further processing
-else:
-    print(f"Request failed with status code {response.status_code}")
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        return result.get('text')
+        # Further processing
+    else:
+        return f"Request failed with status code {response.status_code}"
