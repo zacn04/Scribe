31a90378b6e7bf8a998bc682dc9167879e2c5775
Will Reed <willreed@dhcp-10-29-182-154.dyn.MIT.EDU>
2023-09-17 10:55:42 -0400

    added help window

--- scribe.py
+++ scribe.py
@@ -4,7 +4,6 @@ from PIL import ImageGrab, Image, ImageTk  # Import the ImageGrab module from Pi
 import customtkinter
 import image_processing
 import resizing
-import help_window
 
 # Initialize the main application window
 # root = tk.Tk()
@@ -128,7 +127,7 @@ def end_select(event):
     newSquare = True
     print("updated", x1)
     place_image(screenshot, x0, y0, x1, y1)
-    help_window.main("15 + 7", "22")
+
     
     
     
