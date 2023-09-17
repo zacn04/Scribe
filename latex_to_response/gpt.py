4bb1b8efa7fc1bd36de6b92a917d4b31538b0753
Zac Nwogwugwu <108872007+zacn04@users.noreply.github.com>
2023-09-17 07:40:39 -0400

    Update gpt.py

--- latex_to_response/gpt.py
+++ latex_to_response/gpt.py
@@ -8,9 +8,17 @@ openai.organization = "org-WbwlG5hvut8bXxTOfEm4tfV6"
 openai.api_key = os.getenv(key)
 # openai.Model.list()
 model = "gpt-3.5-turbo-0613"
-query = "I'm so"
 
-print()
-response = openai.Completion.create(model=model, prompt=query)
-print(response)
-print(type(response))
+def explanation(prompt, answer):
+  #This is going to explain the answer given by Wolfram Alpha, based off of the prompt.
+  query = (f'I am going to give you a prompt that has been passed to Wolfram Alpha. Your task is to explain the answer given by Wolfram Alpha. Do not invent math.\n'
+  f'Why is {prompt} equivalent to {answer}? Be concise, answer in less than 300 characters.\n')           
+  #print()
+  response = openai.Completion.create(model=model, prompt=query)
+  #print(response)
+  #print(type(response))
+  return response
+
+# Testing 
+
+print(explanation())
