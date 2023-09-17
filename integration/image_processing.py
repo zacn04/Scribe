5f4b2a45c2f2ec0b0078dee32e6ec7cbf976a2f8
Zac <108872007+zacn04@users.noreply.github.com>
2023-09-17 02:48:51 -0400

    Update image_processing.py

--- integration/image_processing.py
+++ integration/image_processing.py
@@ -1,6 +1,8 @@
 from image_to_latex import image_to_latex
 from latex_to_response import *
 
-def hand_to_hand(x : str) -> str:
+def hand_to_hand(x : str) -> None:
   text = image_to_text(x)
-  
+  text2 = parse(request_query_response(text))
+  hand = text_to_handwriting(your_handwriting, text2) #Function will generate an image that is passed back
+  return hand
