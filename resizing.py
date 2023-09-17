0a8e63c5a714630314e17b02b106196ac14a48bb
Justin Okorie <121595907+jokorie@users.noreply.github.com>
2023-09-17 08:00:58 -0400

    insert image

--- resizing.py
+++ resizing.py
@@ -1,59 +1,81 @@
 import cv2
-# print(cv2.__version__)
-import easyocr
+from PIL import Image
+from PIL.Image import fromarray
 
-# Load the image
-# image = cv2.imread('image_to_latex/test2.png') # 1024 x 192
-image = cv2.imread('latex_to_response/result_image.png') # 200 x 100
-
-
-
-# Define the ROI containing the objects
-# x, y, width, height = 0, 0, 1024, 192  # Adjust these values accordingly
-x, y, width, height = 0, 0, 200, 100  # Adjust these values accordingly
-
-# Crop the ROI from the image
-roi = image[y:y+height, x:x+width]
-for row in roi:
-    print(row)
-
-# Convert the ROI to grayscale (if needed)
-gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-for row in gray_roi:
-    print(row)
-
-# Threshold the grayscale image to segment objects
-_, binary_roi = cv2.threshold(gray_roi, 128, 255, cv2.THRESH_BINARY)
-# cv2.imwrite('roi_image.jpg', binary_roi)
-binary_roi = cv2.bitwise_not(binary_roi)
-# Find contours of objects in the binary image
-contours, _ = cv2.findContours(binary_roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-# Measure the size of each detected object in pixels
-for contour in contours:
-    object_area = cv2.contourArea(contour)
-    object_width = cv2.boundingRect(contour)[2]
-    object_height = cv2.boundingRect(contour)[3]
+def find_four_edges_of_text(image, highlighted_coords, notepad_coords):
+    """find the 4 edges of text within text box corners
+    """    
+    h_top, h_bottom, h_left, h_right = highlighted_coords
+    n_top, n_bottom, n_left, n_right = notepad_coords
     
-    # Print the size of the object in pixels
-    print(f"Object Area: {object_area} pixels^2")
-    print(f"Object Width: {object_width} pixels")
-    print(f"Object Height: {object_height} pixels")
-    print()
 
-# reader = easyocr.Reader(['en'])  # Replace 'en' with the language you're working with
-# results = reader.readtext(image)
-# top = 0
-# bottom = 0
-# for (bbox, text, prob) in results:
-#     (top_left, top_right, bottom_right, bottom_left) = bbox
-#     top = max(top, top_left)
-#     bottom = min(bottom, bottom_left)
-#     top_left = tuple(map(int, top_left))
-#     bottom_right = tuple(map(int, bottom_right))
+    # Define the ROI containing the objects
+    # x, y, width, height = 0, 0, 1024, 192  # Adjust these values accordingly
 
-#     # Draw a bounding box around the character using OpenCV
-#     cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
-# print(top, bottom)
-# cv2.imwrite('image.jpg', image)
-# cv2.imshow('Handwritten Character with Bounding Boxes', image)
+    # Crop the ROI from the image
+    roi = image[h_top:h_bottom, h_left:h_right]
+
+    # Convert the ROI to grayscale (if needed)
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    # Threshold the grayscale image to segment objects
+    _, binary_roi = cv2.threshold(gray_roi, 128, 255, cv2.THRESH_BINARY)
+
+    binary_roi = cv2.bitwise_not(binary_roi)
+    # Find contours of objects in the binary image
+    contours, _ = cv2.findContours(binary_roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # Measure the size of each detected object in pixels
+    top, bottom, left, right = n_bottom, n_top, n_right, n_left
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        t = y
+        b = y + h
+        l = x
+        r = x + w
+
+        top = min(top, t)
+        bottom = max(bottom, b)
+        left = min(left, l)
+        right = max(right, r)
+
+        # object_area = cv2.contourArea(contour)
+        # object_width = cv2.boundingRect(contour)[2]
+        # object_height = cv2.boundingRect(contour)[3]
+        
+        # # Print the size of the object in pixels
+        # print(f"Object Area: {object_area} pixels^2")
+        # print(f"Object Width: {object_width} pixels")
+        # print(f"Object Height: {object_height} pixels")
+    return (top, bottom, left, right)
+
+def resize_image(object_coords, image):
+    height, width = image.shape[:2]
+    aspect_ratio = width / height
+    desired_height = object_coords[1] - object_coords[0]
+    desired_width = int(desired_width*aspect_ratio)
+    output_image = cv2.resize(image, (desired_width, desired_height))
+    return output_image
+
+def find_open_spot(notepad_coords, object_coords, image):
+    # image_dim = l x r
+    o_top, o_bottom, o_left, o_right = object_coords
+    n_top, n_bottom, n_left, n_right = notepad_coords
+    height, width = image.shape[:2]
+    if n_right - o_right >= width:
+        return (o_top, o_bottom, o_left, width)
+    return (o_top-height, o_bottom-height, o_left, width)
+
+def cv_to_pil(cv_image):
+    numpy_array = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(numpy_array)
+    return pil_image
+
+def insert_image(image_filename, highlighted_coords, notepad_coords):
+    image = cv2.imread(image_filename) 
+    object_coords = find_four_edges_of_text(image, highlighted_coords, notepad_coords)
+    resized_image = resize_image(object_coords, image)
+    open_coords = find_open_spot(notepad_coords, object_coords, resized_image)
+    pil_image = cv_to_pil(image)
+    return (open_coords, pil_image)
+    
