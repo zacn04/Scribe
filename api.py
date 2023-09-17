848cef23f0b9a877ef816f84bbce148a509da7aa
Zac <108872007+zacn04@users.noreply.github.com>
2023-09-17 00:05:30 -0400

    Commenting api.py

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+
+#This code makes an API request to Mathpix, which can generate LaTeX from handwritten images.
 url = 'https://api.mathpix.com/v3/text'
 headers = {
     "app_key": "692e9c0662a2d7f5d7ba246d6e9833d5536d63defb1305b3af2e80ff8ce5a894"
